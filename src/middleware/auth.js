d1b3bca16c85e39fd33bf32e4c6ad623ea73d3e3
Vulpie <wojtas_f@outlook.com>
2019-12-06 02:32:54 +0100

    auth.js fix

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,9 @@ const User = require('../models/user')
 
 const auth = async (req,res,next)=>{
     try {
+        
         const token = req.header('Authorization').replace('Bearer ', '')
+        console.log('elo',token)
         const decoded = jwt.verify(token,'thisismynewcourse')
         const user = await User.findOne({
             _id: decoded._id,
